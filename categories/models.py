c988c99286061374910ea6d9a764d4efb0119154
Cristina Lare <100524474+cristinalare@users.noreply.github.com>
2024-11-20 18:30:57 +0200

    fix categories plural name

--- categories/models.py
+++ categories/models.py
@@ -13,6 +13,7 @@ class Category(models.Model):
 
     class Meta:
         ordering = ('-created_at',)
+        verbose_name_plural = "Categories"
 
     def __str__(self):
         return self.title
